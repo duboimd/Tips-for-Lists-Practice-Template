20c3f997ae7b30964f4044f749e451c621a38b64
Matthew DuBois <147000029+duboimd@users.noreply.github.com>
2024-10-24 08:15:18 -0600

    Create Tips-for-Lists.py

---
+++ Tips-for-Lists.py
@@ -0,0 +1,38 @@
+# Q1: Create a list of 100 “hello!” (hint: use multiplication)
+
+
+
+
+# Q2: Combine list1 and list2 into a new list called list3 (hint: use addition). Then, print list3.
+list1 = ["pumpkin", "halloween", "jack-o-lanter", "spooky"]
+list2 = ["christmas", "new year", "holiday", "santa", “snow”]
+
+
+
+
+# Q3: Do the following operations on lst:
+# a. Print the length of the list
+# b. Print index 3
+# c. Print the last item (use negative indexing)
+# d. Append your name to the end of lst. 
+# e. Remove "soccer" from the list.
+# f. Insert a random word at the beginning of the list.
+
+lst = ["cupcakes", "soccer", "pizza", "chocolate", "books"]
+
+
+
+
+
+# Q4: Create a list of 5 of your favourite foods called favFoods.
+# a. Print the length of the list
+# b. Print index 2
+# c. Print the 1 item (use negative indexing)
+# d. Append your name to it
+# e. Insert your age at index 2
+# f. Remove one item from your list
+
+
+
+
+
